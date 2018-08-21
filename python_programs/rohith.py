dad02992559bf6038e7eec5417971fe2b9338929
Rohith Kumar <rohithkumar8084@gmail.com>
2018-08-21 15:31:19 +0530

    deleted commented lines

--- python_programs/rohith.py
+++ python_programs/rohith.py
@@ -4,17 +4,14 @@ bob = var.brk(2,3)
 san = var.kln(3,4)
 print(bob)
 print(san)
-#import leela.a
-#print(a)
-#print(brk)
+
 
 def giri(c,g):
     z =c**g
     #print(z)
     return z
 
-#a=bob
-#b=san
+
 h = giri(c=bob,g=san)
 
 print(h)
