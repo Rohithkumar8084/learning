10fb35c7ee8d8a15d70d0fa48ba947dec35802a4
Rohith Kumar <rohithkumar8084@gmail.com>
2018-08-21 15:27:16 +0530

    python program

---
+++ python_programs/rohith.py
@@ -0,0 +1,20 @@
+from python_programs.New_folder import leela
+var=leela.cbr()
+bob = var.brk(2,3)
+san = var.kln(3,4)
+print(bob)
+print(san)
+#import leela.a
+#print(a)
+#print(brk)
+
+def giri(c,g):
+    z =c**g
+    #print(z)
+    return z
+
+#a=bob
+#b=san
+h = giri(c=bob,g=san)
+
+print(h)
